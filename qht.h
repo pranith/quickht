b2851eff793d7f2ad109e3b4443370e1b84a0817
Pranith Kumar <bobby.prani@gmail.com>
2016-11-10 00:55:43 -0500

    Add missing header

---
+++ qht.h
@@ -0,0 +1,30 @@
+#ifndef __QHT__
+#define __QHT__
+
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "seqlock.h"
+
+#define PER_BUCKET 6
+
+typedef struct qht_bucket {
+  pthread_mutex_t   lock;
+  seqlock_t         seqlock;
+  uint32_t          keys[PER_BUCKET];
+  void              *values[PER_BUCKET];
+  struct qht_bucket *next;
+} qht_bucket;
+
+typedef struct qht {
+  qht_bucket *buckets;
+  uint32_t num_buckets;
+} qht;
+
+qht *qht_init(uint32_t nb);
+void *qht_lookup(qht *table, uint32_t hash);
+bool qht_insert(qht *table, void *value, uint32_t key);
+bool qht_delete(qht *table, uint32_t hash);
+
+#endif
